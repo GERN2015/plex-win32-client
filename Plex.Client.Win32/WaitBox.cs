8a9e5316e2be5e8cceb7284814db74b9110d3e0a
drensin <drensin@30c67751-e462-4b5d-de1d-7152f27224dc>
2010-09-25 03:14:46 +0000

    Fixed race conditions
    Added a delayed progress window for long HTTP requests

--- Plex.Client.Win32/WaitBox.cs
+++ Plex.Client.Win32/WaitBox.cs
@@ -14,24 +14,32 @@ namespace Plex.Client.Win32
         public WaitBox()
         {
             InitializeComponent();
+
+            timer1.Enabled = false;
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            this.label1.Text += ".";
+            if (this.Visible == false)
+                this.Show();
+
+            this.label1.Text = "." + label1.Text + ".";
         }
 
         private void WaitBox_VisibleChanged(object sender, EventArgs e)
         {
-            if (this.Visible == false)
-            {
-                this.timer1.Enabled = false;
-                this.label1.Text = "Getting data";
-            }
-            else
-            {
-                this.timer1.Enabled = true;
-            }
+        }
+
+        public void Start()
+        {
+            this.timer1.Enabled = true;
+        }
+
+        public void Stop()
+        {
+            this.timer1.Enabled = false;
+            this.label1.Text = "Getting data";
+            this.Hide();
         }
     }
 }
